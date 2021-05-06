69e8cb7e1bab003f32fcb88c92df1de31473e10d
amar-rokade <rokade6117@gmail.com>
2021-05-06 22:45:09 +0530

    secrete_key

--- shopping_web/settings.py
+++ shopping_web/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'your secrete key'
+SECRET_KEY = 'YJ0X6yC44g7uCrzi3PeEkQzytBNwv7qomAibXl5noJ-8LTQyoNt0m05CSvEwRfoQDZANqs-1sRXAqElxrXWmZdTrTeAjHG0K01SINo_Qee6G0i-b_idvHnmtzDAA1b2icwT7cA'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
